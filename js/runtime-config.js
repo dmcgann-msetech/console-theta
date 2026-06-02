97f483a21291c51f16859d1ffe352dbeafd0e7b0
Darren McGann <dmcgann@msetech.org>
2026-06-02 07:23:33 -0400

    Externalize Theta Supabase runtime configuration

---
+++ js/runtime-config.js
@@ -0,0 +1,4 @@
+﻿window.THETA_CONFIG = {
+  supabaseUrl: 'https://gaolcfupyanbtskamdll.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdhb2xjZnVweWFuYnRza2FtZGxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3OTU4ODYsImV4cCI6MjA5MjM3MTg4Nn0.ssQHUxoqTGht_nrj-B6q7p9_XhDi1PT6mNp_WtV9HVU'
+};
